b9341bc4a426bd9c557782e2973e50dd02322f7f
adamatusz <adamatusz@gmail.com>
2022-05-14 17:05:23 +0200

    new_database_13052022

--- new_database.py
+++ new_database.py
@@ -1,3 +1,6 @@
+import psycopg2
+from psycopg2 import connect, OperationalError
+from psycopg2.errors import DuplicateDatabase, DuplicateTable
 # new database in sql_server
 
 new_database = "CREATE DATABASE workshop;"
@@ -15,3 +18,40 @@ messages_table = """CREATE TABLE messages(
                         to_id INTEGER REFERENCES users(id) ON DELETE CASCADE, 
                         text varchar(255),
                         creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"""
+
+# connection data with the postgres server
+
+DB_PASSWORD = "Zaq123edc#"
+HOST = "127.0.0.1"
+
+try:
+    _connect = psycopg2.connect(dbname="workshop", user="postgres", password=DB_PASSWORD)
+    _connect.autocommit = True
+    cursor = _connect.cursor()
+# _connect.close()
+
+    try:
+     cursor.execute(new_users_table)
+     print("Database created")
+    except DuplicateDatabase as e:
+     print("Database exists ", e)
+    _connect.close()
+except OperationalError as e:
+    print("Connection Error: ", e)
+
+
+    try:
+        _connect = psycopg2.connect(dbname="workshop", user="postgres", password=DB_PASSWORD, host = HOST)
+        _connect.autocommit = True
+        cursor = _connect.cursor()
+
+        try:
+            cursor.execute(messages_table)
+            print("Messages table created")
+        except DuplicateTable as e:
+            print("Table exists ", e)
+        _connect.close()
+    except OperationalError as e:
+        print("Connection Error", e)
+
+# cursor.execute(messages_table)
